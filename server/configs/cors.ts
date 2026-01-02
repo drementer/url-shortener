8370193487f58d5a67fffc209d501e5420afd55b
Burak <48869708+drementer@users.noreply.github.com>
2026-01-02 20:14:17 +0300

    Update CORS configuration: Add DELETE method to allowed methods for enhanced API functionality.

--- server/configs/cors.ts
+++ server/configs/cors.ts
@@ -2,7 +2,7 @@ import type { CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
   origin: process.env.CLIENT_URL || 'http://localhost:8080',
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   credentials: true,
 };
 
