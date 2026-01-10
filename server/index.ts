531ee0ee12f994ce376311978cf7458f86afd455
drementer <48869708+drementer@users.noreply.github.com>
2026-01-10 20:08:17 +0300

    refactor(server): rename server import to app for clarity in entry point

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import server from './server';
+import app from './server';
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
 
